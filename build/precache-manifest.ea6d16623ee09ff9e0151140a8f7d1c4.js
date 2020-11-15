565963f6d7207982810e03b9bc6a8ab92a1cacab
jackfperryjr <jackfperryjr@gmail.com>
2020-11-14 22:15:34 -0500

    Pulled CNAME and pushed latest build.

---
+++ build/precache-manifest.ea6d16623ee09ff9e0151140a8f7d1c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ffaa6d34af9bf40233ef4d26ae474e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26af6b653d94c3579e26",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "29c2c3f5730594723b8f",
+    "url": "/static/css/main.cb604b5a.chunk.css"
+  },
+  {
+    "revision": "26af6b653d94c3579e26",
+    "url": "/static/js/2.5d580080.chunk.js"
+  },
+  {
+    "revision": "e908922da528485359b3012549bf3244",
+    "url": "/static/js/2.5d580080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c2c3f5730594723b8f",
+    "url": "/static/js/main.fef5e447.chunk.js"
+  },
+  {
+    "revision": "47ffafe0fa4bfbf045de",
+    "url": "/static/js/runtime-main.8a85b754.js"
+  },
+  {
+    "revision": "d8c8ce4f761204162eea1176b38be722",
+    "url": "/static/media/moogle.d8c8ce4f.png"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/static/media/no-image.f98b32ce.png"
+  },
+  {
+    "revision": "d8fe446d9944307273cabbbb638205f9",
+    "url": "/static/media/selphie.d8fe446d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/selphie/precache-manifest.15226967cd76685ea050824707a62cd9.js"
+  "/precache-manifest.ea6d16623ee09ff9e0151140a8f7d1c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/selphie/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
