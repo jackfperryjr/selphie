f323dd9859a272f7d9d9c3b65a2dcc6eea545153
jackfperryjr <jackfperryjr@gmail.com>
2021-01-06 09:08:16 +0000

    gh-pages

---
+++ precache-manifest.9e54eaf1f5b7133854e2108e56864b3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67b425e183cb017a680851b33bb6fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11059b1d7bf6dc92cc52",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "03fbf7797b7fffd8f868",
+    "url": "/static/css/main.e9e3cff3.chunk.css"
+  },
+  {
+    "revision": "11059b1d7bf6dc92cc52",
+    "url": "/static/js/2.6fb2a0a6.chunk.js"
+  },
+  {
+    "revision": "607ccb8e5cfe57a5e67c7962c2feae47",
+    "url": "/static/js/2.6fb2a0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fbf7797b7fffd8f868",
+    "url": "/static/js/main.efe820cd.chunk.js"
+  },
+  {
+    "revision": "47ffafe0fa4bfbf045de",
+    "url": "/static/js/runtime-main.8a85b754.js"
+  },
+  {
+    "revision": "d8c8ce4f761204162eea1176b38be722",
+    "url": "/static/media/moogle.d8c8ce4f.png"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/static/media/no-image.f98b32ce.png"
+  },
+  {
+    "revision": "9a7a153756c37bb7a63403629a399691",
+    "url": "/static/media/selphie.9a7a1537.png"
+  }
+]);
